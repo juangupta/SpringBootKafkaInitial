e8770b0021b19d27a2232bd35fe0f78c2222ebb8
juagomez <juagomez@172.31.18.164>
2018-12-19 10:57:31 -0500

    Initial

--- src/main/java/com/co/bancolombia/juang/api/UserController.java
+++ src/main/java/com/co/bancolombia/juang/api/UserController.java
@@ -1,27 +1,19 @@
 package com.co.bancolombia.juang.api;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.co.bancolombia.juang.model.User;
-import com.co.bancolombia.juang.sender.Sender;
 
 @RestController
 @RequestMapping("kafka")
 public class UserController {
 
-	@Autowired
-    Sender sender;
-
-	private static final String TOPIC = "test";
-
 	@GetMapping("/publish/{name}")
 	public String post(@PathVariable("name") final String name) {
-		sender.send(new User(name, "Technology", 12000L) );
+		User user = new User(name, "Technology", 12000L);
 		return "Published successfully: " + name;
 	}
 
